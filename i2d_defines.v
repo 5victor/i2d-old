dce7b1ad0b0f11b94c43e99940f4ff1421015854
Victor Wen <vic7tor@gmail.com>
2011-12-13 18:30:29 +0800

    add some code

--- i2d_defines.v
+++ i2d_defines.v
@@ -45,19 +45,22 @@
 
 //branch instruction may have 2 opcodes(imm, reg), while the condition in ins[26:0]
 `define I2D_INS_B	6'b010110
-`define I2D_INS_BR	6'b010111
+`define I2D_INS_BI	6'b010111
+
+/*
 `define I2D_INS_BE	6'b011000
 `define I2D_INS_BER	6'b011001
 `define I2D_INS_BNE	6'b011010
 `define I2D_INS_BNER	6'b011011
-`define I2D_INS_B	6'b011100
-`define I2D_INS_BR	6'b011101
-`define I2D_INS_B	6'b011110
-`define I2D_INS_BR	6'b011111
-`define I2D_INS_B	6'b100000
-`define I2D_INS_BR	6'b100001
-`define I2D_INS_B	6'b100010
-`define I2D_INS_BR	6'b100011
+`define I2D_INS_BG	6'b011100
+`define I2D_INS_BGR	6'b011101
+`define I2D_INS_BL	6'b011110
+`define I2D_INS_BLR	6'b011111
+`define I2D_INS_BA	6'b100000
+`define I2D_INS_BAR	6'b100001
+`define I2D_INS_BB	6'b100010
+`define I2D_INS_BBR	6'b100011
+*/
 
 //other instruction
 `define I2D_INS_NOP	6'b100100
@@ -65,9 +68,51 @@
 `define I2D_INS_MOVI	6'b100110
 `define I2D_INS_SWI	6'b100111
 `define I2D_INS_CALL	6'b101000
-`define	I2D_INS_CALLR	6'b110110
+`define	I2D_INS_CALLI	6'b110110
 `define I2D_INS_RET	6'b101001
-`define I2D_INS_RETE	6'b101010
+`define I2D_INS_RFE	6'b101010
 `define I2D_INS_MSR	6'b101011
 `define I2D_INS_MRS	6'b101100
+
+//i2d regfile
+`define I2D_GPR_WIDTH	4
+
+`define I2D_RF_R0	4'b0000
+
+//mode depend gpr
+`define I2D_RF_LR	4'b1101
+`define I2D_RF_SP	4'b1110
+`define I2D_RF_PC	4'b1111
+
+//mode depend spr
+`define I2D_RF_EPC	5'b10000
+`define I2D_RF_ESR	5'b10001
+
+//oprand mux
+`define I2D_OPMUX_A_NONE	0
+`define I2D_OPMUX_A_RA		1
+`define I2D_OPMUX_A_ID_PC	2
+`define I2D_OPMUX_B_NONE	0
+`define I2D_OPMUX_B_RB		1
+`define I2D_OPMUX_B_ID_PC	2
+`define I2D_OPMUX_B_IMM		3
+
+//alu op
+`define I2D_ALUOP_NONE		0
+`define I2D_ALUOP_ADD		1
+`define I2D_ALUOP_ADDC		2
+`define I2D_ALUOP_SUB		3
+`define I2D_ALUOP_SUBC		4
+`define I2D_ALUOP_MUL		5
+`define I2D_ALUOP_MULU		6
+`define I2D_ALUOP_DIV		7
+`define I2D_ALUOP_DIVU		8
+`define I2D_ALUOP_AND		9
+`define I2D_ALUOP_OR		8
+`define I2D_ALUOP_NOT		9
+`define I2D_ALUOP_RSL		10
+`define I2D_ALUOP_RSR		11
+`define I2D_ALUOP_ASL		12
+`define I2D_ALUOP_ASR		13
+`define I2D_ALUOP_ERR		14
 
--- i2d_id.v
+++ i2d_id.v
@@ -12,7 +12,8 @@ module i2d_id(
 	//input
 	if_ins, if_pc, sr, id_dis,
 	//output
-	id_ins, id_pc, imm, rf_ra, rf_rb, rf_r, alu_op, id_err, swi, branch
+	id_ins, id_pc, imm, rfa_addr, rfb_addr, rfa_r, rfb_r, alu_op, id_err,
+	swi, branch, oprand_mux_a, oprand_mux_b
 );
 
 input		clk;
@@ -25,30 +26,43 @@ input		id_dis;
 output	[31:0]	id_ins;
 output	[31:0]	id_pc;
 output	[31:0]	imm;
-output	[5:0]	rf_ra;
-output	[5:0]	rf_rb;
-output		rf_r;
+output	[5:0]	rfa_addr;
+output	[5:0]	rfb_addr;
+output		rfa_r;
+output		rfb_r;
 output	[3:0]	alu_op;
 output		id_err;
 output		swi;
 output		branch;
+output	[1:0]	oprand_mux_a;
+output	[1:0]	oprand_mux_b;
 
 reg	[31:0]	id_ins;
 reg	[31:0]	id_pc;
 reg	[31:0]	imm;
-reg	[5:0]	rf_ra;
-reg	[5:0]	rf_rb;
-reg		rf_r;
+reg	[3:0]	rfa_addr;
+reg	[3:0]	rfb_addr;
+reg		rfa_r;
+reg		rfb_r;
 reg	[3:0]	alu_op;
-reg		id_err;
+//reg		id_err;
 reg		swi;
 reg		branch;
+reg	[1:0]	oprand_mux_a;
+reg	[1:0]	oprand_mux_b;
 
 //internal
-reg		sel_imm_r;	//instruction have a register operand
-reg		sel_imm_nr;	//instruction does not have a register operand
+reg		sel_imm_16;	//16 bit imm
+reg		sel_imm_22;	//22 bit imm
+reg		imm_ext;	//imm sign extend
+reg		oprand_ra_pc;	//oprand mux
+reg		oprand_rb_pc;	//
 reg		sel_rf_ra;
 reg		sel_rf_rb;
+
+//reg		sel_rf_swi;
+//reg		sel_rf_ret;
+//reg		sel_rf_rfe;
 
 always @(posedge clk)
 begin
@@ -65,15 +79,36 @@ begin
 end
 
 
-always @(posedge clk)
+always @(imm_ext or sel_imm_16 or sel_imm_22)
 begin
-	if (~id_dis)
-		if (sel_imm_nr)
-			imm <= {7{id_ins[25]}, id_ins[24:0]};
-		if (sel_imm_r)
-			imm <= {12{id_ins[20], id_ins[19:0]};
-	else
+	if (~rst)
 		imm <= 0;
+	else
+	begin
+		if (~id_dis)
+		begin
+			if (imm_ext)
+			begin
+				if (sel_imm_16)
+					imm <= {{17{id_ins[15]}}, id_ins[14:0]};
+				else if (sel_imm_22)
+					imm <= {{11{id_ins[21]}}, id_ins[20:0]};
+				else
+					imm <= 0;
+			end
+			else
+			begin
+				if (sel_imm_16)
+					imm <= id_ins[15:0];
+				else if (sel_imm_22)
+					imm <= id_ins[21:0];
+				else
+					imm <= 0;
+			end
+		end
+		else
+			imm <= 0;
+	end
 end
 
 //decode imm
@@ -81,34 +116,240 @@ always @(id_ins)
 begin
 	case (id_ins[31:26])
 		//ALU
-		`I2D_INS_ADDI:
-		`I2D_INS_ADDCI:
-		`I2D_INS_SUBI:
-		`I2D_INS_SUBCI:
-		`I2D_INS_MULI:
-		`I2D_INS_MULUI:
-		`I2D_INS_DIVI:
-		`I2D_INS_DIVUI:
-		`I2D_INS_ANDI:
-		`I2D_INS_ORI:
-		`I2D_INS_RSLI:
-		`I2D_INS_RSRI:
-		`I2D_INS_ASLI:
+		`I2D_INS_ADDI,
+		`I2D_INS_ADDCI,
+		`I2D_INS_SUBI,
+		`I2D_INS_SUBCI,
+		`I2D_INS_MULI,
+		`I2D_INS_DIVI,
+		`I2D_INS_ANDI,
+		`I2D_INS_ORI,
+		`I2D_INS_MOVI,
+		`I2D_INS_LD,
+		`I2D_INS_ST:
+		begin
+			sel_imm_16 <= 1;
+			sel_imm_22 <= 0;
+			imm_ext <= 1;
+		end
+		//usigned
+		`I2D_INS_MULUI,
+		`I2D_INS_DIVUI,
+		`I2D_INS_RSLI,
+		`I2D_INS_RSRI,
+		`I2D_INS_ASLI,
 		`I2D_INS_ASRI:
-		//other
-		`I2D_INS_MOVI:	sel_imm_r <= 1;
+		begin
+			sel_imm_16 <= 1;
+			sel_imm_22 <= 0;
+			imm_ext <= 0;
+		end
 		//branch
-		`I2D_INS_B:
+		`I2D_INS_BI,
 		//OTHER
-		`I2D_INS_CALL:	sel_imm_nr <= 1;		
-		default:	sel_imm <= 0;
+		`I2D_INS_CALLI:
+		begin
+			sel_imm_16 <= 0;
+			sel_imm_22 <= 1;
+			imm_ext <= 1;
+		end
+		default:
+			begin
+			sel_imm_16 <= 0;
+			sel_imm_22 <= 0;
+			imm_ext <= 0;
+			end
 	endcase
 end
 
 //decode rf
+always @(sel_rf_ra or sel_rf_rb)
+begin
+	if (sel_rf_ra)
+		if (if_ins[21:18] == `I2D_RF_PC)
+		begin
+			oprand_ra_pc <= 1;
+			rfa_addr <= 0;
+			rfa_r <= 0;
+		end
+		else
+		begin
+			oprand_ra_pc <= 0;
+			rfa_addr <= if_ins[21:18];
+			rfa_r <= 1;
+		end
+	else
+	begin
+		oprand_ra_pc <= 0;
+		rfa_addr <= 0;
+		rfa_r <= 0;
+	end
+
+	if (sel_rf_rb)
+		if (if_ins[17:14] == `I2D_RF_PC)
+		begin
+			oprand_rb_pc <= 1;
+			rfb_addr <= 0;
+			rfb_r <= 0;
+		end
+		else
+		begin
+			oprand_rb_pc <= 0;
+			rfb_addr <= if_ins[17:14];
+			rfb_r <= 1;
+		end
+	else
+	begin
+		oprand_rb_pc <= 0;
+		rfb_addr <= 0;
+		rfb_r <= 0;
+	end
+end
+
+always @(if_ins)
+begin
+	if (rst)
+	begin
+		sel_rf_ra <= 0;
+		sel_rf_rb <= 0;
+	end
+	else
+	case (if_ins[31:26])
+		`I2D_INS_ADD,
+		`I2D_INS_ADDC,
+		`I2D_INS_SUB,
+		`I2D_INS_SUBC,
+		`I2D_INS_MUL,
+		`I2D_INS_MULU,
+		`I2D_INS_DIV,
+		`I2D_INS_DIVU,
+		`I2D_INS_AND,
+		`I2D_INS_OR,
+		`I2D_INS_RSL,
+		`I2D_INS_ASL,
+		`I2D_INS_ASR:
+		//`I2D_INS_RETE:
+		begin
+			sel_rf_ra <= 1;
+			sel_rf_rb <= 1;
+		end
+		`I2D_INS_NOT,
+		`I2D_INS_B,
+		`I2D_INS_CALL,
+		`I2D_INS_MOV,
+		`I2D_INS_ST,
+		//
+		`I2D_INS_RET,
+		`I2D_INS_MRS:
+		begin
+			sel_rf_ra <= 1;
+			sel_rf_rb <= 0;
+		end
+		default:
+		begin
+			sel_rf_ra <= 0;
+			sel_rf_rb <= 0;
+		end
+	endcase
+end
 
 
-	
+//oprand mux
+
+always @(sel_rf_ra or sel_rf_rb or sel_imm_16 or sel_imm_22 or oprand_ra_pc or oprand_rb_pc)
+begin
+	if (sel_rf_ra)
+		oprand_mux_a <= `I2D_OPMUX_A_RA;
+	else if (oprand_ra_pc)
+		oprand_mux_a <= `I2D_OPMUX_A_ID_PC;
+	else
+		oprand_mux_a <= `I2D_OPMUX_A_NONE;
+
+	if (sel_rf_rb)
+		oprand_mux_b <= `I2D_OPMUX_B_RB;
+	else if (oprand_rb_pc)
+		oprand_mux_b <= `I2D_OPMUX_B_ID_PC;
+	else if (sel_imm_16 | sel_imm_22)
+		oprand_mux_b <= `I2D_OPMUX_B_IMM;
+	else
+		oprand_mux_b <= `I2D_OPMUX_B_NONE;
+
+end
+
+//decode aluop & id_err(illgel instruction)
+
+assign id_err = alu_op == `I2D_ALUOP_ERR ? 1 : 0;
+
+always @(id_ins)
+begin
+	if (~rst)
+		alu_op <= `I2D_ALUOP_NONE;
+	else
+		case (id_ins[31:26])
+			`I2D_INS_ADD,
+			`I2D_INS_ADDI,
+			`I2D_INS_LD,
+			`I2D_INS_ST,
+			`I2D_INS_B,
+			`I2D_INS_BI,
+			`I2D_INS_CALL,
+			`I2D_INS_CALLI:
+				alu_op <= `I2D_ALUOP_ADD;
+			`I2D_INS_ADDC,
+			`I2D_INS_ADDCI:
+				alu_op <= `I2D_ALUOP_ADDC;
+			`I2D_INS_SUB,
+			`I2D_INS_SUBI:
+				alu_op <= `I2D_ALUOP_SUB;
+			`I2D_INS_SUBC,
+			`I2D_INS_SUBCI:
+				alu_op <= `I2D_ALUOP_SUBC;
+			`I2D_INS_MUL,
+			`I2D_INS_MULI:
+				alu_op <= `I2D_ALUOP_MUL;
+			`I2D_INS_MULU,
+			`I2D_INS_MULUI:
+				alu_op <= `I2D_ALUOP_MULU;
+			`I2D_INS_DIV,
+			`I2D_INS_DIVI:
+				alu_op <= `I2D_ALUOP_DIV;
+			`I2D_INS_DIVU,
+			`I2D_INS_DIVUI:
+				alu_op <= `I2D_ALUOP_DIVU;
+			`I2D_INS_AND,
+			`I2D_INS_ANDI:
+				alu_op <= `I2D_ALUOP_AND;
+			`I2D_INS_OR,
+			`I2D_INS_ORI:
+				alu_op <= `I2D_ALUOP_OR;
+			`I2D_INS_NOT,
+			`I2D_INS_NOTI:
+				alu_op <= `I2D_ALUOP_NOT;
+			`I2D_INS_RSL,
+			`I2D_INS_RSLI:
+				alu_op <= `I2D_ALUOP_RSL;
+			`I2D_INS_RSR,
+			`I2D_INS_RSRI:
+				alu_op <= `I2D_ALUOP_RSR;
+			`I2D_INS_ASL,
+			`I2D_INS_ALLI:
+				alu_op <= `I2D_ALUOP_ASL;
+			`I2D_INS_ASR,
+			`I2D_INS_ASRI:
+				alu_op <= `I2D_ALUOP_ASR;
+			`I2D_INS_NOP,
+			`I2D_INS_MOV,
+			`I2D_INS_MOVI,
+			`I2D_INS_SWI,
+			`I2D_INS_RET,
+			`I2D_INS_RFE,
+			`I2D_INS_MSR,
+			`I2D_INS_MRS:
+				alu_op <= `I2D_ALUOP_NONE;
+			default:
+				alu_op <= `I2D_ALUOP_ERR;
+		endcase
+end
 
 endmodule
 
