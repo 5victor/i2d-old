7eb6c97fb2b81a1780da1702c504e2467c4aa9de
Victor Wen <vic7tor@gmail.com>
2011-12-16 00:50:15 +0800

    adding i2d_ex

---
+++ i2d_ex.v
@@ -0,0 +1,21 @@
+/* filename
+ *
+ * Copyright: Victor Wen, vic7tor@gmail.com
+ * This code publish under GNU GPL License
+ *
+ * i2d ex module
+ */
+
+`include "i2d_defines.v"
+
+module i2d_ex(
+	rst, clk,
+	//input
+	id_ins, id_pc, ex_dis,
+	//ouput
+	mau_op, sel_mau_addr
+);
+
+
+endmodule
+
