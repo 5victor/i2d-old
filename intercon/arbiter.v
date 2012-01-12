98872e4107642216061fec32a156c74eafec2242
Victor Wen <vic7tor@gmail.com>
2012-01-12 19:27:36 +0800

    change layout, add intercon

---
+++ intercon/arbiter.v
@@ -0,0 +1,10 @@
+/* filename
+ *
+ * Copyright: Victor Wen, vic7tor@gmail.com
+ * This code publish under GNU GPL License
+ *
+ * i2d soc intercon arbiter module
+ */
+
+`include "i2d_soc_defines.v"
+
